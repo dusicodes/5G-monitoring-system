e3b4873302cd22361489f38e840bbbc0c1c677d0
Endurance Aloja <alojaeddie@gmail.com>
2024-11-11 06:58:23 +0000

    Initialise 5g Monitoring App

---
+++ 5G-Monitoring-app/5G-Monitoring-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  5G-Monitoring-app
+//
+//  Created by Eddie Aloja on 11/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 5G-Monitoring-app/5G-Monitoring-app/_G_Monitoring_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  _G_Monitoring_appApp.swift
+//  5G-Monitoring-app
+//
+//  Created by Eddie Aloja on 11/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct _G_Monitoring_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
