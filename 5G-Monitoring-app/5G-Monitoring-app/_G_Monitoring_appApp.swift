4e6ca0ac957b4bfc2701d9c61b86ca8141d79609
Endurance Aloja <alojaeddie@gmail.com>
2024-11-13 22:14:37 +0000

    add gid sign in delegate

--- 5G-Monitoring-app/5G-Monitoring-app/_G_Monitoring_appApp.swift
+++ 5G-Monitoring-app/5G-Monitoring-app/_G_Monitoring_appApp.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 import Firebase
 import FirebaseCore
+import GoogleSignIn
 
 
 
@@ -21,16 +22,23 @@ struct _G_Monitoring_appApp: App {
     }
 }
 
+
 class AppDelegate: NSObject, UIApplicationDelegate {
     
-    func application(_ application: UIApplication,
-                     
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         
         FirebaseApp.configure()
         print("Fire base works")
         return true
         
     }
+    
+    func application(_ app: UIApplication,
+                     open url: URL,
+                     options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+      return GIDSignIn.sharedInstance.handle(url)
+    }
+    
+    
     
 }
