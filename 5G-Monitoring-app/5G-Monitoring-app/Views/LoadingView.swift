e3982bba8425688b76e4105633c703d9ef26e7b8
Endurance Aloja <alojaeddie@gmail.com>
2024-11-16 23:38:44 +0000

    Create loading view

---
+++ 5G-Monitoring-app/5G-Monitoring-app/Views/LoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingView.swift
+//  5G-Monitoring-app
+//
+//  Created by Eddie Aloja on 16/11/2024.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
